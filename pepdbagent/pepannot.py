58ab34cc89152b961a1dd3b62ed278a4e5a4af17
Khoroshevskyi <sasha99250@gmail.com>
2022-11-09 15:54:20 -0500

    Fixed annotation error

--- pepdbagent/pepannot.py
+++ pepdbagent/pepannot.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from typing import Optional
 
 
@@ -11,3 +11,10 @@ class Annotation(BaseModel):
 
     class Config:
         allow_population_by_field_name = True
+        validate_assignment = True
+
+    @validator("is_private", pre=True, always=True)
+    def set_name(cls, is_private):
+        if is_private is None:
+            return False
+        return is_private
