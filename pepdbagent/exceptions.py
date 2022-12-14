a78d6ccf6633bbca3b01f015226d0802cf220045
Khoroshevskyi <sasha99250@gmail.com>
2022-12-14 14:57:02 -0500

    deleted unused exception

--- pepdbagent/exceptions.py
+++ pepdbagent/exceptions.py
@@ -10,11 +10,3 @@ class SchemaError(Exception):
         super().__init__(
             """PEP_db connection error! The schema of connected db is incorrect"""
         )
-
-
-class DeleteError(Exception):
-    def __init__(self):
-        # Call the base class constructor with the parameters it needs
-        super().__init__(
-            """PEP_db connection error! The schema of connected db is incorrect"""
-        )
